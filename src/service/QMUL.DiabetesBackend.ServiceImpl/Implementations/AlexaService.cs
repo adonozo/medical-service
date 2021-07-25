122bbc41165288a3f6bfbdfa2c36870d5c3b2111
Alvaro Donozo <ar.donozo@acad.ucb.edu.bo>
2021-07-26 00:21:02 +0100

    Break Alexa requests into methods based by type in Alexa Service

--- src/service/QMUL.DiabetesBackend.ServiceImpl/Implementations/AlexaService.cs
+++ src/service/QMUL.DiabetesBackend.ServiceImpl/Implementations/AlexaService.cs
@@ -1,7 +1,10 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Hl7.Fhir.Model;
 using QMUL.DiabetesBackend.DataInterfaces;
+using QMUL.DiabetesBackend.Model;
 using QMUL.DiabetesBackend.Model.Enums;
 using QMUL.DiabetesBackend.ServiceInterfaces;
 
@@ -28,20 +31,59 @@ namespace QMUL.DiabetesBackend.ServiceImpl.Implementations
         public async Task<Bundle> ProcessRequest(string patientEmailOrId, AlexaRequestType type, DateTime dateTime, AlexaRequestTime requestTime,
             CustomEventTiming timing)
         {
+            switch (type)
+            {
+                case AlexaRequestType.Medication:
+                    return await this.GetMedicationRequests(patientEmailOrId, dateTime, requestTime, timing);
+                case AlexaRequestType.Insulin:
+                    break;
+                case AlexaRequestType.Glucose:
+                    break;
+                case AlexaRequestType.Appointment:
+                    break;
+                case AlexaRequestType.CarePlan:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(type), type, null);
+            }
+            
+            throw new NotImplementedException();
+        }
+
+        public async Task<Bundle> GetMedicationRequests(string patientEmailOrId, DateTime dateTime, AlexaRequestTime requestTime,
+            CustomEventTiming timing)
+        {
             var patient = await this.patientDao.GetPatientByIdOrEmail(patientEmailOrId);
+            IEnumerable<HealthEvent> events;
             switch (requestTime)
             {
                 case AlexaRequestTime.ExactTime:
+                    events = await this.eventDao.GetEvents(patient.Id.ToString(), EventType.MedicationDosage, dateTime, timing);
                     break;
                 case AlexaRequestTime.AllDay:
-                    break;
+                    throw new NotImplementedException();
                 case AlexaRequestTime.OnEvent:
-                    var events = this.eventDao.GetEvents(patient.Id.ToString(), dateTime, timing);
+                    events = await this.eventDao.GetEvents(patient.Id.ToString(), EventType.MedicationDosage, dateTime, timing);
                     break;
                 default:
                     throw new ArgumentOutOfRangeException(nameof(requestTime), requestTime, null);
             }
-            
+
+            var bundle = GenerateEmptyBundle();
+            var medicationRequests = await this.GetMedicationBundle(events);
+            bundle.Entry = medicationRequests.Select(request => new Bundle.EntryComponent{ Resource = request}).ToList();
+            return bundle;
+        }
+
+        public Task<Bundle> GetServiceRequests(string patientEmailOrId, DateTime dateTime, AlexaRequestTime requestTime,
+            CustomEventTiming timing)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Bundle> GetMeasurements(string patientEmailOrId, DateTime dateTime, AlexaRequestTime requestTime,
+            CustomEventTiming timing)
+        {
             throw new NotImplementedException();
         }
 
@@ -50,141 +92,20 @@ namespace QMUL.DiabetesBackend.ServiceImpl.Implementations
             throw new NotImplementedException();
         }
 
-        private Tuple<DateTime, DateTime> GetTimeInterval(DateTime dateTime, AlexaRequestTime requestTime,
-            Timing.EventTiming timing)
+        private static Bundle GenerateEmptyBundle()
         {
-            var startTime = dateTime;
-            var endTime = dateTime;
-            switch (requestTime)
+            return new()
             {
-                case AlexaRequestTime.AllDay:
-                    startTime = startTime.Date;
-                    endTime = startTime.AddDays(1);
-                    break;
-                // TODO
-                // case AlexaRequestTime.ExactTime:
-                //     break;
-                // case AlexaRequestTime.OnEvent:
-                //     break;
-            }
-
-            return new Tuple<DateTime, DateTime>(startTime, endTime);
+                Type = Bundle.BundleType.Searchset,
+                Timestamp = DateTimeOffset.UtcNow
+            };
         }
 
-        private Tuple<DateTime, DateTime> GetIntervalFromEventTiming(DateTime startTime, Timing.EventTiming timing)
+        private async Task<List<MedicationRequest>> GetMedicationBundle(IEnumerable<HealthEvent> events)
         {
-            DateTime endTime;
-            switch (timing)
-            {
-                case Timing.EventTiming.MORN:
-                    startTime = startTime.Date.AddHours(6);
-                    endTime = startTime.Date.AddHours(12);
-                    break;
-                case Timing.EventTiming.MORN_early:
-                    startTime = startTime.Date.AddHours(6);
-                    endTime = startTime.Date.AddHours(9);
-                    break;
-                case Timing.EventTiming.MORN_late:
-                    startTime = startTime.Date.AddHours(9);
-                    endTime = startTime.Date.AddHours(12);
-                    break;
-                case Timing.EventTiming.NOON:
-                    startTime = startTime.Date.AddHours(11).AddMinutes(30);
-                    endTime = startTime.Date.AddHours(12).AddMinutes(30);
-                    break;
-                case Timing.EventTiming.AFT:
-                    startTime = startTime.Date.AddHours(12);
-                    endTime = startTime.Date.AddHours(18);
-                    break;
-                case Timing.EventTiming.AFT_early:
-                    startTime = startTime.Date.AddHours(12);
-                    endTime = startTime.Date.AddHours(15);
-                    break;
-                case Timing.EventTiming.AFT_late:
-                    startTime = startTime.Date.AddHours(15);
-                    endTime = startTime.Date.AddHours(18);
-                    break;
-                case Timing.EventTiming.EVE:
-                    startTime = startTime.Date.AddHours(6);
-                    endTime = startTime.Date.AddHours(9);
-                    break;
-                case Timing.EventTiming.EVE_early:
-                    startTime = startTime.Date.AddHours(12);
-                    endTime = startTime.Date.AddHours(18);
-                    break;
-                case Timing.EventTiming.EVE_late:
-                    startTime = startTime.Date.AddHours(9);
-                    endTime = startTime.Date.AddHours(12);
-                    break;
-                case Timing.EventTiming.NIGHT:
-                    startTime = startTime.Date.AddHours(18);
-                    endTime = startTime.Date.AddHours(30);
-                    break;
-                case Timing.EventTiming.PHS:
-                    startTime = startTime.Date.AddHours(12);
-                    endTime = startTime.Date.AddHours(18);
-                    break;
-                case Timing.EventTiming.HS:
-                    startTime = startTime.Date.AddHours(12);
-                    endTime = startTime.Date.AddHours(18);
-                    break;
-                case Timing.EventTiming.WAKE:
-                    startTime = startTime.Date.AddHours(12);
-                    endTime = startTime.Date.AddHours(18);
-                    break;
-                case Timing.EventTiming.C:
-                    startTime = startTime.Date.AddHours(12);
-                    endTime = startTime.Date.AddHours(18);
-                    break;
-                case Timing.EventTiming.CM:
-                    startTime = startTime.Date.AddHours(12);
-                    endTime = startTime.Date.AddHours(18);
-                    break;
-                case Timing.EventTiming.CD:
-                    startTime = startTime.Date.AddHours(12);
-                    endTime = startTime.Date.AddHours(18);
-                    break;
-                case Timing.EventTiming.CV:
-                    startTime = startTime.Date.AddHours(12);
-                    endTime = startTime.Date.AddHours(18);
-                    break;
-                case Timing.EventTiming.AC:
-                    startTime = startTime.Date.AddHours(12);
-                    endTime = startTime.Date.AddHours(18);
-                    break;
-                case Timing.EventTiming.ACM:
-                    startTime = startTime.Date.AddHours(12);
-                    endTime = startTime.Date.AddHours(18);
-                    break;
-                case Timing.EventTiming.ACD:
-                    startTime = startTime.Date.AddHours(12);
-                    endTime = startTime.Date.AddHours(18);
-                    break;
-                case Timing.EventTiming.ACV:
-                    startTime = startTime.Date.AddHours(12);
-                    endTime = startTime.Date.AddHours(18);
-                    break;
-                case Timing.EventTiming.PC:
-                    startTime = startTime.Date.AddHours(12);
-                    endTime = startTime.Date.AddHours(18);
-                    break;
-                case Timing.EventTiming.PCM:
-                    startTime = startTime.Date.AddHours(12);
-                    endTime = startTime.Date.AddHours(18);
-                    break;
-                case Timing.EventTiming.PCD:
-                    startTime = startTime.Date.AddHours(12);
-                    endTime = startTime.Date.AddHours(18);
-                    break;
-                case Timing.EventTiming.PCV:
-                    startTime = startTime.Date.AddHours(12);
-                    endTime = startTime.Date.AddHours(18);
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(timing), timing, null);
-            }
-
-            return new Tuple<DateTime, DateTime>(startTime, endTime);
+            var uniqueIds = new HashSet<string>();
+            uniqueIds.UnionWith(events.Select(item => item.Resource.ResourceId).ToArray());
+            return await this.medicationRequestDao.GetMedicationRequestsByIds(uniqueIds.ToArray());
         }
     }
 }
--- src/service/QMUL.DiabetesBackend.ServiceInterfaces/IAlexaService.cs
+++ src/service/QMUL.DiabetesBackend.ServiceInterfaces/IAlexaService.cs
@@ -10,6 +10,27 @@ namespace QMUL.DiabetesBackend.ServiceInterfaces
         public Task<Bundle> ProcessRequest(string patientEmailOrId, AlexaRequestType type, DateTime dateTime,
             AlexaRequestTime requestTime, CustomEventTiming timing);
 
+        /// <summary>
+        /// Gets the medication requests for a given time.
+        /// </summary>
+        /// <returns>A Bundle list containing <see cref="MedicationRequest"/></returns>
+        public Task<Bundle> GetMedicationRequests(string patientEmailOrId, DateTime dateTime,
+            AlexaRequestTime requestTime, CustomEventTiming timing);
+
+        /// <summary>
+        /// Gets the service requests for a given time.
+        /// </summary>
+        /// /// <returns>A Bundle list containing <see cref="ServiceRequest"/></returns>
+        public Task<Bundle> GetServiceRequests(string patientEmailOrId, DateTime dateTime,
+            AlexaRequestTime requestTime, CustomEventTiming timing);
+
+        /// <summary>
+        /// Gets the patient's blood glucose measures for a given time 
+        /// </summary>
+        /// <returns>A Bundle list containing <see cref="Measure"/></returns>
+        public Task<Bundle> GetMeasurements(string patientEmailOrId, DateTime dateTime,
+            AlexaRequestTime requestTime, CustomEventTiming timing);
+
         public DiagnosticReport SaveGlucoseMeasure(string patientId, DiagnosticReport report);
     }
 }
