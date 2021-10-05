6c0eaa83787cc7306f6ab9f32544a2bccee71392
Alvaro Donozo <ar.donozo@acad.ucb.edu.bo>
2021-10-05 16:48:47 +0100

    Lint request models from the controllers

--- src/api/QMUL.DiabetesBackend.Controllers/Models/PatientStartDateRequest.cs
+++ src/api/QMUL.DiabetesBackend.Controllers/Models/PatientStartDateRequest.cs
@@ -1,9 +1,15 @@
-using System;
-
 namespace QMUL.DiabetesBackend.Api.Models
 {
+    using System;
+
+    /// <summary>
+    /// The model to get the start date from a request body
+    /// </summary>
     public class PatientStartDateRequest
     {
+        /// <summary>
+        /// The request start date
+        /// </summary>
         public DateTime StartDate { get; set; }
     }
 }
--- src/api/QMUL.DiabetesBackend.Controllers/Models/PatientTimingRequest.cs
+++ src/api/QMUL.DiabetesBackend.Controllers/Models/PatientTimingRequest.cs
@@ -1,12 +1,21 @@
-using System;
-using QMUL.DiabetesBackend.Model.Enums;
-
 namespace QMUL.DiabetesBackend.Api.Models
 {
+    using System;
+    using Model.Enums;
+
+    /// <summary>
+    /// The patient's request to update a timing value, e.g., breakfast 
+    /// </summary>
     public class PatientTimingRequest
     {
+        /// <summary>
+        /// The custom event timing to update
+        /// </summary>
         public CustomEventTiming Timing { get; set; }
         
+        /// <summary>
+        /// The datetime for the timing event
+        /// </summary>
         public DateTime DateTime { get; set; }
     }
 }
