db1180972d29bc9e19e7485f3b09b35d87d8932a
adonozo <ar.donozo@acad.ucb.edu.bo>
2021-11-16 19:41:08 +0000

    Add log messages and comments for CarePlanService.cs

--- src/core/service/QMUL.DiabetesBackend.ServiceImpl/Implementations/CarePlanService.cs
+++ src/core/service/QMUL.DiabetesBackend.ServiceImpl/Implementations/CarePlanService.cs
@@ -5,6 +5,7 @@ namespace QMUL.DiabetesBackend.ServiceImpl.Implementations
     using System.Threading.Tasks;
     using DataInterfaces;
     using Hl7.Fhir.Model;
+    using Microsoft.Extensions.Logging;
     using ServiceInterfaces;
     using Utils;
 
@@ -17,42 +18,50 @@ namespace QMUL.DiabetesBackend.ServiceImpl.Implementations
         private readonly IMedicationRequestDao medicationRequestDao;
         private readonly IServiceRequestDao serviceRequestDao;
         private readonly IPatientDao patientDao;
+        private readonly ILogger<CarePlanService> logger;
 
         public CarePlanService(IServiceRequestDao serviceRequestDao,
-            IMedicationRequestDao medicationRequestDao, IPatientDao patientDao)
+            IMedicationRequestDao medicationRequestDao, IPatientDao patientDao, ILogger<CarePlanService> logger)
         {
             this.serviceRequestDao = serviceRequestDao;
             this.medicationRequestDao = medicationRequestDao;
             this.patientDao = patientDao;
+            this.logger = logger;
         }
 
-        /// <inheritdoc/>>
+        /// <inheritdoc/>
         public async Task<Bundle> GetActiveCarePlans(string patientIdOrEmail)
         {
+            this.logger.LogTrace("Getting active care plans for {IdOrEmail}", patientIdOrEmail);
             var patient = await this.patientDao.GetPatientByIdOrEmail(patientIdOrEmail);
             var medicationRequests = await this.medicationRequestDao.GetAllActiveMedicationRequests(patient.Id);
             var serviceRequests = await this.serviceRequestDao.GetActiveServiceRequests(patient.Id);
             var bundle = ResourceUtils.GenerateEmptyBundle();
             bundle.Entry = medicationRequests.Select(request => new Bundle.EntryComponent {Resource = request})
                 .ToList();
+            this.logger.LogTrace("Found {Count} medication requests", medicationRequests.Count);
             bundle.Entry.AddRange(serviceRequests.Select(request => new Bundle.EntryComponent {Resource = request})
                 .ToList());
+            this.logger.LogTrace("Found {Count} service requests", serviceRequests.Count);
             return bundle;
         }
 
-        /// <inheritdoc/>>
+        /// <inheritdoc/>
         public async Task<Bundle> GetCarePlanFor(string patientEmailOrId)
         {
+            this.logger.LogTrace("Getting care plans for {IdOrEmail}", patientEmailOrId);
             var patient = await ResourceUtils.ValidateObject(
                 () => this.patientDao.GetPatientByIdOrEmail(patientEmailOrId),
-                "Unable to find patient for the Observation", new KeyNotFoundException());
+                "Unable to find patient.", new KeyNotFoundException());
             var medicationRequests = await this.medicationRequestDao.GetMedicationRequestFor(patient.Id);
             var serviceRequests = await this.serviceRequestDao.GetServiceRequestsFor(patient.Id);
             var bundle = ResourceUtils.GenerateEmptyBundle();
             bundle.Entry = medicationRequests.Select(request => new Bundle.EntryComponent {Resource = request})
                 .ToList();
+            this.logger.LogTrace("Found {Count} medication requests", medicationRequests.Count);
             bundle.Entry.AddRange(serviceRequests.Select(request => new Bundle.EntryComponent {Resource = request})
                 .ToList());
+            this.logger.LogTrace("Found {Count} service requests", serviceRequests.Count);
             return bundle;
         }
     }
