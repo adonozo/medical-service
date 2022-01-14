4a1bff79aff9ae9c2390dd1a2da4d05c3436d1c7
adonozo <ar.donozo@acad.ucb.edu.bo>
2022-01-14 23:37:18 +0000

    Update ObservationService to use the exception handler

--- src/core/service/QMUL.DiabetesBackend.ServiceInterfaces/IObservationService.cs
+++ src/core/service/QMUL.DiabetesBackend.ServiceInterfaces/IObservationService.cs
@@ -2,6 +2,7 @@ namespace QMUL.DiabetesBackend.ServiceInterfaces
 {
     using System;
     using System.Threading.Tasks;
+    using Exceptions;
     using Hl7.Fhir.Model;
     using Model.Enums;
 
@@ -16,6 +17,8 @@ namespace QMUL.DiabetesBackend.ServiceInterfaces
         /// <param name="patientId">The patient's ID or email.</param>
         /// <param name="newObservation">The <see cref="Observation"/> to create.</param>
         /// <returns>The <see cref="Observation"/> created with a new ID.</returns>
+        /// <exception cref="NotFoundException">If the patient was not found.</exception>
+        /// <exception cref="CreateException">If the observation could not be created.</exception>
         public Task<Observation> CreateObservation(string patientId, Observation newObservation);
 
         /// <summary>
@@ -23,6 +26,7 @@ namespace QMUL.DiabetesBackend.ServiceInterfaces
         /// </summary>
         /// <param name="observationId">The observation ID to look for.</param>
         /// <returns>A single <see cref="Observation"/> object if found. An error otherwise.</returns>
+        /// <exception cref="NotFoundException">If the observation was not found.</exception>
         public Task<Observation> GetSingleObservation(string observationId);
 
         /// <summary>
@@ -30,6 +34,7 @@ namespace QMUL.DiabetesBackend.ServiceInterfaces
         /// </summary>
         /// <param name="patientId">The patient ID who owns the observations.</param>
         /// <returns>The list of <see cref="Observation"/> for a patient in a <see cref="Bundle"/> object.</returns>
+        /// <exception cref="NotFoundException">If the patient was not found.</exception>
         public Task<Bundle> GetAllObservationsFor(string patientId);
 
         /// <summary>
@@ -41,6 +46,7 @@ namespace QMUL.DiabetesBackend.ServiceInterfaces
         /// <param name="dateTime">The <see cref="DateTime"/> when the observation(s) happened.</param>
         /// <param name="patientTimezone">The patient's timezone.</param>
         /// <returns>A <see cref="Bundle"/> object with the list of observations.</returns>
+        /// <exception cref="NotFoundException">If the patient was not found.</exception>
         public Task<Bundle> GetObservationsFor(string patientId, CustomEventTiming timing, DateTime dateTime,
             string patientTimezone = "UTC");
     }
