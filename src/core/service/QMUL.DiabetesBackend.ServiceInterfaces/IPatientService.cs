9b324e0c3e18f42c083a7e40f7bc43efa5ae5a12
adonozo <ar.donozo@acad.ucb.edu.bo>
2022-01-14 23:41:40 +0000

    Update PatientService to use the exception handler

--- src/core/service/QMUL.DiabetesBackend.ServiceInterfaces/IPatientService.cs
+++ src/core/service/QMUL.DiabetesBackend.ServiceInterfaces/IPatientService.cs
@@ -2,6 +2,7 @@ namespace QMUL.DiabetesBackend.ServiceInterfaces
 {
     using System.Collections.Generic;
     using System.Threading.Tasks;
+    using Exceptions;
     using Patient = Model.Patient;
 
     /// <summary>
@@ -20,8 +21,15 @@ namespace QMUL.DiabetesBackend.ServiceInterfaces
         /// </summary>
         /// <param name="newPatient">The new <see cref="Patient"/> to create.</param>
         /// <returns>The created <see cref="Patient"/> with a new ID.</returns>
+        /// <exception cref="CreateException">If the patient could not be created.</exception>
         public Task<Patient> CreatePatient(Patient newPatient);
 
+        /// <summary>
+        /// Gets a single patient given an Id or email.
+        /// </summary>
+        /// <param name="idOrEmail">The patient's ID or email</param>
+        /// <returns>The <see cref="Patient"/></returns>
+        /// <exception cref="NotFoundException">If the patient was not found.</exception>
         public Task<Patient> GetPatient(string idOrEmail);
     }
 }
