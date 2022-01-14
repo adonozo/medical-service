0557fbcfa96d51bd1758e0b934adeee78e7f8a3e
adonozo <ar.donozo@acad.ucb.edu.bo>
2022-01-14 00:14:19 +0000

    Update ObservationDao to use data exceptions

--- src/data/QMUL.DiabetesBackend.MongoDb/ObservationDao.cs
+++ src/data/QMUL.DiabetesBackend.MongoDb/ObservationDao.cs
@@ -4,9 +4,9 @@ namespace QMUL.DiabetesBackend.MongoDb
     using System.Collections.Generic;
     using System.Threading.Tasks;
     using DataInterfaces;
+    using DataInterfaces.Exceptions;
     using Hl7.Fhir.Model;
     using Microsoft.Extensions.Logging;
-    using Model;
     using Models;
     using MongoDB.Driver;
     using Utils;
@@ -19,9 +19,9 @@ namespace QMUL.DiabetesBackend.MongoDb
         private readonly IMongoCollection<MongoObservation> observationCollection;
         private const string CollectionName = "observation";
         private readonly ILogger<ObservationDao> logger;
-        
+
         /// <inheritdoc />
-        public ObservationDao(IDatabaseSettings settings, ILogger<ObservationDao> logger) : base(settings)
+        public ObservationDao(IMongoDatabase database, ILogger<ObservationDao> logger) : base(database)
         {
             this.logger = logger;
             this.observationCollection = this.Database.GetCollection<MongoObservation>(CollectionName);
@@ -31,10 +31,15 @@ namespace QMUL.DiabetesBackend.MongoDb
         public async Task<Observation> CreateObservation(Observation observation)
         {
             this.logger.LogDebug("Creating observation");
-            var mongoObservation = observation.ToMongoObservation();
-            await this.observationCollection.InsertOneAsync(mongoObservation);
-            this.logger.LogDebug("Observation created with ID: {Id}", mongoObservation.Id);
-            return await this.GetObservation(mongoObservation.Id);
+            var newMongoObservation = observation.ToMongoObservation();
+            await this.observationCollection.InsertOneAsync(newMongoObservation);
+            this.logger.LogDebug("Observation created with ID: {Id}", newMongoObservation.Id);
+            const string errorMessage = "Could not create observation";
+            return await this.GetSingleOrThrow(this.observationCollection
+                    .Find(mongoObservation => mongoObservation.Id == newMongoObservation.Id)
+                    .Project(mongoObservation => mongoObservation.ToObservation()),
+                new CreateException(errorMessage),
+                () => this.logger.LogWarning(errorMessage));
         }
 
         /// <inheritdoc />
@@ -42,7 +47,9 @@ namespace QMUL.DiabetesBackend.MongoDb
         {
             var cursor = this.observationCollection.Find(observation => observation.Id == observationId)
                 .Project(mongoObservation => mongoObservation.ToObservation());
-            return await cursor.FirstOrDefaultAsync();
+            var errorMessage = $"Could not find observation with ID {observationId}";
+            return await this.GetSingleOrThrow(cursor, new NotFoundException(errorMessage),
+                () => this.logger.LogWarning("{ErrorMessage}", errorMessage));
         }
 
         /// <inheritdoc />
