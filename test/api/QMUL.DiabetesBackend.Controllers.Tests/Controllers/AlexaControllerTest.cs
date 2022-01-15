fc3ff72d76bfb85255c7d02b262f5d99f54366b5
adonozo <ar.donozo@acad.ucb.edu.bo>
2022-01-15 00:40:32 +0000

    Use exception handler in AlexaController

--- test/api/QMUL.DiabetesBackend.Controllers.Tests/Controllers/AlexaControllerTest.cs
+++ test/api/QMUL.DiabetesBackend.Controllers.Tests/Controllers/AlexaControllerTest.cs
@@ -1,7 +1,6 @@
 namespace QMUL.DiabetesBackend.Controllers.Tests.Controllers
 {
     using System;
-    using System.Collections.Generic;
     using Api.Controllers;
     using FluentAssertions;
     using Hl7.Fhir.Model;
@@ -12,11 +11,92 @@ namespace QMUL.DiabetesBackend.Controllers.Tests.Controllers
     using NSubstitute;
     using NSubstitute.ExceptionExtensions;
     using ServiceInterfaces;
+    using ServiceInterfaces.Exceptions;
     using Xunit;
     using Task = System.Threading.Tasks.Task;
 
     public class AlexaControllerTest
     {
+        [Fact]
+        public async Task GetMedicationRequest_WhenRequestIsCorrect_ReturnsStatusOk()
+        {
+            // Arrange
+            var service = Substitute.For<IAlexaService>();
+            var logger = Substitute.For<ILogger<AlexaController>>();
+            service.ProcessMedicationRequest(Arg.Any<string>(), Arg.Any<DateTime>(),
+                    Arg.Any<CustomEventTiming>(), Arg.Any<string>())
+                .Returns(new Bundle());
+            var controller = new AlexaController(service, logger);
+
+            // Act
+            var result = await controller.GetMedicationRequest("test@mail.com", DateTime.Now,
+                default, CustomEventTiming.ALL_DAY);
+            var status = (ObjectResult) result;
+
+            // Assert
+            status.StatusCode.Should().Be(StatusCodes.Status200OK);
+        }
+
+        [Fact]
+        public async Task GetInsulinMedicationRequest_WhenRequestIsCorrect_ReturnsStatusOk()
+        {
+            // Arrange
+            var service = Substitute.For<IAlexaService>();
+            var logger = Substitute.For<ILogger<AlexaController>>();
+            service.ProcessInsulinMedicationRequest(Arg.Any<string>(), Arg.Any<DateTime>(),
+                    Arg.Any<CustomEventTiming>(), Arg.Any<string>())
+                .Returns(new Bundle());
+            var controller = new AlexaController(service, logger);
+
+            // Act
+            var result = await controller.GetInsulinMedicationRequest("test@mail.com", DateTime.Now,
+                default, CustomEventTiming.ALL_DAY);
+            var status = (ObjectResult) result;
+
+            // Assert
+            status.StatusCode.Should().Be(StatusCodes.Status200OK);
+        }
+
+        [Fact]
+        public async Task GetGlucoseServiceRequest_WhenRequestIsCorrect_ReturnsStatusOk()
+        {
+            // Arrange
+            var service = Substitute.For<IAlexaService>();
+            var logger = Substitute.For<ILogger<AlexaController>>();
+            service.ProcessGlucoseServiceRequest(Arg.Any<string>(), Arg.Any<DateTime>(),
+                    Arg.Any<CustomEventTiming>(), Arg.Any<string>())
+                .Returns(new Bundle());
+            var controller = new AlexaController(service, logger);
+
+            // Act
+            var result = await controller.GetGlucoseServiceRequest("test@mail.com", DateTime.Now,
+                default, CustomEventTiming.ALL_DAY);
+            var status = (ObjectResult) result;
+
+            // Assert
+            status.StatusCode.Should().Be(StatusCodes.Status200OK);
+        }
+
+        [Fact]
+        public async Task GetCarePlan_WhenRequestIsCorrect_ReturnsStatusOk()
+        {
+            // Arrange
+            var service = Substitute.For<IAlexaService>();
+            var logger = Substitute.For<ILogger<AlexaController>>();
+            service.ProcessCarePlanRequest(Arg.Any<string>(), Arg.Any<DateTime>(),
+                    Arg.Any<CustomEventTiming>(), Arg.Any<string>())
+                .Returns(new Bundle());
+            var controller = new AlexaController(service, logger);
+
+            // Act
+            var result = await controller.GetCarePlan("test@mail.com", DateTime.Now,
+                default, CustomEventTiming.ALL_DAY);
+            var status = (ObjectResult) result;
+
+            // Assert
+            status.StatusCode.Should().Be(StatusCodes.Status200OK);
+        }
+
         [Fact]
         public async Task GetAlexaNextRequest_WhenRequestIsCorrect_ReturnsStatusOk()
         {
@@ -33,7 +113,7 @@ namespace QMUL.DiabetesBackend.Controllers.Tests.Controllers
             // Assert
             status.StatusCode.Should().Be(StatusCodes.Status200OK);
         }
-        
+
         [Fact]
         public async Task GetAlexaNextRequest_WhenNotFound_ReturnsNotFound()
         {
@@ -41,7 +121,7 @@ namespace QMUL.DiabetesBackend.Controllers.Tests.Controllers
             var service = Substitute.For<IAlexaService>();
             var logger = Substitute.For<ILogger<AlexaController>>();
             service.GetNextRequests(Arg.Any<string>(), Arg.Any<AlexaRequestType>())
-                .Throws(new KeyNotFoundException());
+                .Throws(new NotFoundException(string.Empty));
             var controller = new AlexaController(service, logger);
 
             // Act
@@ -51,7 +131,7 @@ namespace QMUL.DiabetesBackend.Controllers.Tests.Controllers
             // Assert
             status.StatusCode.Should().Be(StatusCodes.Status404NotFound);
         }
-        
+
         [Fact]
         public async Task GetAlexaNextRequest_WhenException_ReturnsInternalError()
         {
