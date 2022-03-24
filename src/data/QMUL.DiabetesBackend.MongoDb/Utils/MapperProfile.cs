0f25088178f738f6ec4b71d5c2852bb97874f3fa
adonozo <donozo.alvaro@gmail.com>
2022-03-24 23:02:59 +0000

    Add automapper in dao

---
+++ src/data/QMUL.DiabetesBackend.MongoDb/Utils/MapperProfile.cs
@@ -0,0 +1,22 @@
+namespace QMUL.DiabetesBackend.MongoDb.Utils
+{
+    using AutoMapper;
+    using Hl7.Fhir.Model;
+    using Model;
+    using Models;
+
+    public class MapperProfile : Profile
+    {
+        public MapperProfile()
+        {
+            CreateMap<Patient, MongoPatient>()
+                .ForMember(mongoPatient => mongoPatient.Email, 
+                    opt => opt.MapFrom(patient => patient.GetEmailExtension()))
+                .ForMember(mongoPatient => mongoPatient.ExactEventTimes,
+                    opt => opt.MapFrom(patient => patient.GetTimingPreference()))
+                .ReverseMap()
+                .ForSourceMember(mongoPatient => mongoPatient.Email, opt => opt.DoNotValidate())
+                .ForSourceMember(mongoPatient => mongoPatient.ExactEventTimes, opt => opt.DoNotValidate());
+        }
+    }
+}
