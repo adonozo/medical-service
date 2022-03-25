1cd643da4e6399bab26486fcb044ef63c93c542f
adonozo <donozo.alvaro@gmail.com>
2022-03-25 23:52:32 +0000

    Remove MongoServiceRequest and its references

--- src/data/QMUL.DiabetesBackend.MongoDb/ServiceRequestDao.cs
+++ src/data/QMUL.DiabetesBackend.MongoDb/ServiceRequestDao.cs
@@ -7,36 +7,32 @@ namespace QMUL.DiabetesBackend.MongoDb
     using DataInterfaces.Exceptions;
     using Hl7.Fhir.Model;
     using Microsoft.Extensions.Logging;
-    using Models;
+    using Model.Constants;
     using MongoDB.Driver;
-    using Utils;
 
     /// <summary>
     /// The Service Request Dao
     /// </summary>
     public class ServiceRequestDao : MongoDaoBase, IServiceRequestDao
     {
-        // TODO stop using MongoServiceRequest
-        private readonly IMongoCollection<MongoServiceRequest> serviceRequestCollection;
+        private readonly IMongoCollection<ServiceRequest> serviceRequestCollection;
         private const string CollectionName = "serviceRequest";
         private readonly ILogger<ServiceRequestDao> logger;
 
         public ServiceRequestDao(IMongoDatabase database, ILogger<ServiceRequestDao> logger) : base(database)
         {
             this.logger = logger;
-            this.serviceRequestCollection = this.Database.GetCollection<MongoServiceRequest>(CollectionName);
+            this.serviceRequestCollection = this.Database.GetCollection<ServiceRequest>(CollectionName);
         }
 
         /// <inheritdoc />
         public async Task<ServiceRequest> CreateServiceRequest(ServiceRequest newRequest)
         {
             this.logger.LogDebug("Creating new service request");
-            var mongoRequest = newRequest.ToMongoServiceRequest();
-            mongoRequest.CreateAt = DateTime.UtcNow;
-            await this.serviceRequestCollection.InsertOneAsync(mongoRequest);
-            this.logger.LogDebug("Service request created with ID: {Id}", mongoRequest.Id);
+            await this.serviceRequestCollection.InsertOneAsync(newRequest);
+            this.logger.LogDebug("Service request created with ID: {Id}", newRequest.Id);
             const string exceptionMessage = "Could not create the service request";
-            return await this.GetSingleRequestOrThrow(mongoRequest.Id, new CreateException(exceptionMessage),
+            return await this.GetSingleRequestOrThrow(newRequest.Id, new CreateException(exceptionMessage),
                 () => this.logger.LogWarning("{ExceptionMessage}", exceptionMessage));
         }
 
@@ -52,9 +48,9 @@ namespace QMUL.DiabetesBackend.MongoDb
         public async Task<List<ServiceRequest>> GetServiceRequestsFor(string patientId)
         {
             this.logger.LogTrace("Getting service requests for {PatientId}", patientId);
+            var patientReference = Constants.PatientPath + patientId;
             var result = this.serviceRequestCollection.Find(request =>
-                    request.PatientReference.ReferenceId == patientId)
-                .Project(mongoServiceRequest => mongoServiceRequest.ToServiceRequest());
+                    request.Subject.Reference == patientReference);
             var serviceRequests = await result.ToListAsync();
             this.logger.LogTrace("Found {Count} service request(s) for {Id}", serviceRequests.Count, patientId);
             return serviceRequests;
@@ -64,10 +60,10 @@ namespace QMUL.DiabetesBackend.MongoDb
         public async Task<List<ServiceRequest>> GetActiveServiceRequests(string patientId)
         {
             this.logger.LogTrace("Getting service active service requests for {PatientId}", patientId);
+            var patientReference = Constants.PatientPath + patientId;
             var result = this.serviceRequestCollection.Find(request =>
-                    request.PatientReference.ReferenceId == patientId
-                    && request.Status == RequestStatus.Active.ToString())
-                .Project(mongoServiceRequest => mongoServiceRequest.ToServiceRequest());
+                    request.Subject.Reference == patientReference
+                    && request.Status == RequestStatus.Active);
             var serviceRequests = await result.ToListAsync();
             this.logger.LogTrace("Found {Count} service request(s) for {Id}", serviceRequests.Count, patientId);
             return serviceRequests;
@@ -76,10 +72,9 @@ namespace QMUL.DiabetesBackend.MongoDb
         /// <inheritdoc />
         public async Task<List<ServiceRequest>> GetServiceRequestsByIds(string[] ids)
         {
-            var idFilter = Builders<MongoServiceRequest>.Filter
+            var idFilter = Builders<ServiceRequest>.Filter
                 .In(item => item.Id, ids);
-            var cursor = this.serviceRequestCollection.Find(idFilter)
-                .Project(mongoServiceRequest => mongoServiceRequest.ToServiceRequest());
+            var cursor = this.serviceRequestCollection.Find(idFilter);
             return await cursor.ToListAsync();
         }
 
@@ -87,9 +82,8 @@ namespace QMUL.DiabetesBackend.MongoDb
         public async Task<ServiceRequest> UpdateServiceRequest(string id, ServiceRequest actualRequest)
         {
             logger.LogDebug("Updating service request {Id}", id);
-            var mongoRequests = actualRequest.ToMongoServiceRequest();
             var result =
-                await this.serviceRequestCollection.ReplaceOneAsync(request => request.Id == id, mongoRequests);
+                await this.serviceRequestCollection.ReplaceOneAsync(request => request.Id == id, actualRequest);
             var errorMessage = $"Could not update service request with ID {id}";
             this.CheckAcknowledgedOrThrow(result.IsAcknowledged, new UpdateException(errorMessage),
                 () => this.logger.LogWarning("{ErrorMessage}", errorMessage));
@@ -109,8 +103,7 @@ namespace QMUL.DiabetesBackend.MongoDb
         private async Task<ServiceRequest> GetSingleRequestOrThrow(string id, DataExceptionBase exception,
             Action fallback)
         {
-            var result = this.serviceRequestCollection.Find(request => request.Id == id)
-                .Project(request => request.ToServiceRequest());
+            var result = this.serviceRequestCollection.Find(request => request.Id == id);
             return await this.GetSingleOrThrow(result, exception, fallback);
         }
     }
--- src/data/QMUL.DiabetesBackend.MongoDb/Utils/Mapper.cs
+++ src/data/QMUL.DiabetesBackend.MongoDb/Utils/Mapper.cs
@@ -1,128 +1,13 @@
 namespace QMUL.DiabetesBackend.MongoDb.Utils
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using Hl7.Fhir.Model;
     using Model;
     using Models;
-    using static System.Enum;
-    using ResourceReference = Hl7.Fhir.Model.ResourceReference;
 
     /// <summary>
     /// Maps FHIR objects into custom Mongo objects and vice-versa.
     /// </summary>
     public static class Mapper
     {
-        public static Timing ToTiming(this MongoTiming timing)
-        {
-            var hasPeriodUnit = TryParse<Timing.UnitsOfTime>(timing.PeriodUnit, out var periodUnit);
-            var dayEvent = new List<Timing.EventTiming?>();
-            foreach (var value in timing.When)
-            {
-                var canParse = TryParse<Timing.EventTiming>(value, out var result);
-                if (canParse)
-                {
-                    dayEvent.Add(result);
-                }
-            }
-
-            var daysOfWeek = new List<DaysOfWeek?>();
-            foreach (var day in timing.DaysOfWeek)
-            {
-                var canParse = TryParse<DaysOfWeek>(day, out var result);
-                if (canParse)
-                {
-                    daysOfWeek.Add(result);
-                }
-            }
-
-            DataType bounds;
-            if (timing.PeriodStart != null && timing.PeriodEnd != null)
-            {
-                bounds = new Period
-                {
-                    Start = timing.PeriodStart?.ToString("o"),
-                    End = timing.PeriodEnd?.ToString("o")
-                };
-            }
-            else
-            {
-                bounds = new Duration
-                {
-                    Value = timing.DayDuration,
-                    Unit = "d"
-                };
-            }
-
-            return new Timing
-            {
-                Repeat = new Timing.RepeatComponent
-                {
-                    Bounds = bounds,
-                    Frequency = timing.Frequency,
-                    Period = timing.Period,
-                    PeriodUnit = hasPeriodUnit ? periodUnit : null,
-                    Offset = timing.Offset,
-                    When = dayEvent,
-                    TimeOfDay = timing.TimesOfDay,
-                    DayOfWeek = daysOfWeek
-                }
-            };
-        }
-
-        public static Quantity ToQuantity(this MongoQuantity quantity) =>
-            new()
-            {
-                System = quantity.System,
-                Code = quantity.Code,
-                Unit = quantity.Unit,
-                Value = quantity.Value,
-            };
-
-        public static MongoTiming ToMongoTiming(this Timing timing)
-        {
-            var startTime = (timing.Repeat.Bounds as Period)?.Start;
-            var endTime = (timing.Repeat.Bounds as Period)?.End;
-            var days = 0;
-            if (timing.Repeat.Bounds is Duration duration)
-            {
-                days = duration.Value != null ? (int)duration.Value : 0;
-            }
-
-            var whenList = from eventTiming in timing.Repeat.When
-                where eventTiming != null
-                select eventTiming.ToString();
-            var daysOfWeek = from day in timing.Repeat.DayOfWeek
-                where day != null
-                select day.ToString();
-
-            return new MongoTiming
-            {
-                Frequency = timing.Repeat.Frequency ?? 0,
-                Period = timing.Repeat.Period ?? 0,
-                PeriodUnit = timing.Repeat.PeriodUnit?.ToString(),
-                DayDuration = days,
-                Offset = timing.Repeat.Offset ?? 0,
-                PeriodStart = startTime != null ? DateTime.Parse(startTime) : null,
-                PeriodEnd = endTime != null ? DateTime.Parse(endTime) : null,
-                When = whenList,
-                DaysOfWeek = daysOfWeek,
-                TimesOfDay = timing.Repeat.TimeOfDay.Select(time => time.ToString())
-            };
-        }
-
-        public static MongoQuantity ToMongoQuantity(this Quantity dose)
-        {
-            return new()
-            {
-                Value = dose.Value ?? 0,
-                System = dose.System,
-                Unit = dose.Unit,
-                Code = dose.Code
-            };
-        }
-
         // TODO use mapper instead or stop using MongoEvent
         public static MongoEvent ToMongoEvent(this HealthEvent healthEvent)
         {
@@ -147,35 +32,6 @@ namespace QMUL.DiabetesBackend.MongoDb.Utils
                 EventTiming = mongoEvent.EventTiming,
                 ExactTimeIsSetup = mongoEvent.ExactTimeIsSetup,
                 ResourceReference = mongoEvent.ResourceReference
-            };
-        }
-
-        public static MongoCode ToMongoCode(this Coding code)
-        {
-            return new()
-            {
-                Display = code.Display,
-                Code = code.Code,
-                System = code.System
-            };
-        }
-
-        public static Coding ToCoding(this MongoCode code)
-        {
-            return new()
-            {
-                Display = code.Display,
-                Code = code.Code,
-                System = code.System
-            };
-        }
-
-        public static ResourceReference ToResourceReference(this MongoReference reference)
-        {
-            return new()
-            {
-                ElementId = reference.ReferenceId,
-                Display = reference.ReferenceName,
             };
         }
     }
