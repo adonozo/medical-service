67b7f798d1631126ac55d3787fcc28acd805162f
adonozo <donozo.alvaro@gmail.com>
2022-05-08 20:06:19 +0100

    Remove unused helper method

--- src/api/QMUL.DiabetesBackend.Controllers/Utils/Helpers.cs
+++ src/api/QMUL.DiabetesBackend.Controllers/Utils/Helpers.cs
@@ -1,30 +1,10 @@
 namespace QMUL.DiabetesBackend.Api.Utils
 {
-    using System;
-    using System.Threading.Tasks;
-    using Hl7.Fhir.Model;
-    using Hl7.Fhir.Serialization;
     using Microsoft.AspNetCore.Http;
     using Model;
-    using Newtonsoft.Json.Linq;
 
     public static class Helpers
     {
-        /// <summary>
-        /// Parses a <see cref="JObject"/> object into a <see cref="Resource"/> that can be used.
-        /// </summary>
-        /// <param name="jObject">The object to parse. It should have the shape of a resource, e.g., a patient.</param>
-        /// <typeparam name="T">The resource type to convert into.</typeparam>
-        /// <returns>The converted object</returns>
-        /// <exception cref="FormatException">If the object is malformed.</exception>
-        public static async Task<T> ParseResourceAsync<T>(JObject jObject) where T : Resource
-        {
-            var parser = new FhirJsonParser(new ParserSettings
-                { AllowUnrecognizedEnums = true, AcceptUnknownMembers = false, PermissiveParsing = false });
-            var resource = await parser.ParseAsync<T>(jObject.ToString());
-            return resource;
-        }
-
         /// <summary>
         /// Sets the HTTP response headers for a paginated result.
         /// </summary>
