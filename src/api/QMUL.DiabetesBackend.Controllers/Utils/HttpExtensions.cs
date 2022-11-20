5a097e9ded06945cb3aaf325e59eaa6541f0a579
adonozo <donozo.alvaro@gmail.com>
2022-11-20 18:18:44 +0000

    Add comments to extension method

--- src/api/QMUL.DiabetesBackend.Controllers/Utils/HttpExtensions.cs
+++ src/api/QMUL.DiabetesBackend.Controllers/Utils/HttpExtensions.cs
@@ -20,6 +20,14 @@ namespace QMUL.DiabetesBackend.Api.Utils
             context.Response.Headers[HttpConstants.RemainingCountHeader] = paginatedResult.RemainingCount.ToString();
         }
 
+        /// <summary>
+        /// Checks the <see cref="Resource"/> result to send an OK HTTP result, or a Not Found result if the resource
+        /// is null.
+        /// </summary>
+        /// <param name="controller">The <see cref="Controller"/></param>
+        /// <param name="result">A <see cref="Resource"/> result.</param>
+        /// <typeparam name="T">A Resource type object, e.g., Medication</typeparam>
+        /// <returns>An Action Result of type OK, or NotFound</returns>
         public static IActionResult OkOrNotFound<T>(this ControllerBase controller, T? result) where T: Resource
         {
             if (result is null)
