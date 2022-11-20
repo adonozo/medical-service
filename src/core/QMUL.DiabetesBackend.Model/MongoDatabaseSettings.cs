11d5f9e44fecd1bea174f90328281085592c1d74
adonozo <donozo.alvaro@gmail.com>
2022-11-20 18:18:03 +0000

    Remove unnecessary mongo options interface

--- src/core/QMUL.DiabetesBackend.Model/MongoDatabaseSettings.cs
+++ src/core/QMUL.DiabetesBackend.Model/MongoDatabaseSettings.cs
@@ -3,17 +3,7 @@ namespace QMUL.DiabetesBackend.Model
     /// <summary>
     /// An auxiliary plain class to map database settings with a settings file (appsettings.json).
     /// </summary>
-    public class MongoDatabaseSettings : IDatabaseSettings
-    {
-        public string DatabaseName { get; set; }
-        
-        public string DatabaseConnectionString { get; set; }
-    }
-
-    /// <summary>
-    /// An auxiliary interface to map database settings.
-    /// </summary>
-    public interface IDatabaseSettings
+    public class MongoDatabaseSettings
     {
         public string DatabaseName { get; set; }
         
