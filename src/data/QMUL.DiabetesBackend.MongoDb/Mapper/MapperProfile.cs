797a4c0ae8e230191e293ff144ae7e119d3f7685
Alvaro Donozo <donozo.alvaro@gmail.com>
2023-07-23 20:24:05 +0100

    Remove MongoEvent

--- src/data/QMUL.DiabetesBackend.MongoDb/Mapper/MapperProfile.cs
+++ src/data/QMUL.DiabetesBackend.MongoDb/Mapper/MapperProfile.cs
@@ -2,16 +2,10 @@ namespace QMUL.DiabetesBackend.MongoDb.Mapper;
 
 using AutoMapper;
 using Model;
-using Models;
 
 public class MapperProfile : Profile
 {
     public MapperProfile()
     {
-        CreateMap<ResourceReference, MongoEvent.MongoResourceReference>()
-            .ReverseMap();
-
-        CreateMap<HealthEvent, MongoEvent>()
-            .ReverseMap();
     }
 }
