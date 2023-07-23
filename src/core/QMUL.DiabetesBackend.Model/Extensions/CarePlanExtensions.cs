1af245656de13c2310663f20bad7632fe8d7d01f
Alvaro Donozo <donozo.alvaro@gmail.com>
2023-07-23 18:26:09 +0100

    Add comments to care plan extension methods

--- src/core/QMUL.DiabetesBackend.Model/Extensions/CarePlanExtensions.cs
+++ src/core/QMUL.DiabetesBackend.Model/Extensions/CarePlanExtensions.cs
@@ -6,12 +6,24 @@ using Hl7.Fhir.Model;
 
 public static class CarePlanExtensions
 {
+    /// <summary>
+    /// Gets the IDs of service requests contained in a <see cref="CarePlan"/> activity. It looks in the activity's
+    /// <see cref="ResourceReference"/> field
+    /// </summary>
+    /// <param name="carePlan">The <see cref="CarePlan"/></param>
+    /// <returns>An array of service request IDs</returns>
     public static string[] GetServiceRequestsIds(this CarePlan carePlan) =>
         carePlan.Activity
             .Where(resource => resource.Reference.Reference.Contains(Constants.ServiceRequestPath))
             .Select(resource => resource.Reference.GetIdFromReference())
             .ToArray();
-    
+
+    /// <summary>
+    /// Gets the IDs of medication requests contained in a <see cref="CarePlan"/> activity. It looks in the activity's
+    /// <see cref="ResourceReference"/> field
+    /// </summary>
+    /// <param name="carePlan">The <see cref="CarePlan"/></param>
+    /// <returns>An array of medication request IDs</returns>
     public static string[] GetMedicationRequestsIds(this CarePlan carePlan) =>
         carePlan.Activity
             .Where(resource => resource.Reference.Reference.Contains(Constants.MedicationRequestPath))
