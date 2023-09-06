9cca3bb3f20cd1f54ba7d6b8c383865f874dec90
adonozo <ar.donozo@acad.ucb.edu.bo>
2023-09-06 14:12:45 +0100

    Add integration tests for medication

--- test/integration-tests/QMUL.DiabetesBackend.Integration.Tests/MedicationTests.cs
+++ test/integration-tests/QMUL.DiabetesBackend.Integration.Tests/MedicationTests.cs
@@ -1,18 +1,35 @@
 namespace QMUL.DiabetesBackend.Integration.Tests;
 
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
-using System.Threading.Tasks;
 using FluentAssertions;
+using Hl7.Fhir.Model;
+using Microsoft.Extensions.DependencyInjection;
+using Utils;
 using Xunit;
+using Task = System.Threading.Tasks.Task;
 
-public class MedicationTests : IClassFixture<TestFixture>
+public class MedicationTests : IClassFixture<TestFixture>, IAsyncLifetime
 {
     private readonly HttpClient httpClient;
+    private readonly MongoDBTest mongoDbTest;
 
     public MedicationTests(TestFixture fixture)
     {
         this.httpClient = fixture.CreateClient();
+        var services = fixture.Services;
+        this.mongoDbTest = services.GetRequiredService<MongoDBTest>();
+    }
+
+    public Task InitializeAsync()
+    {
+        return Task.CompletedTask;
+    }
+
+    public async Task DisposeAsync()
+    {
+        await this.mongoDbTest.ResetDatabase(TestFixture.TestDatabase);
     }
 
     [Fact]
@@ -21,4 +38,89 @@ public class MedicationTests : IClassFixture<TestFixture>
         var result = await this.httpClient.GetAsync("medications");
         result.StatusCode.Should().Be(HttpStatusCode.OK);
     }
+
+    [Fact]
+    public async Task CreateMedication_ReturnsObject()
+    {
+        // Arrange
+        var medication = this.GetTestMedication();
+
+        // Act
+        var createResponse = await this.httpClient.PostResource("medications", medication);
+
+        // Assert
+        createResponse.StatusCode.Should().Be(HttpStatusCode.OK);
+        var createResult = await HttpUtils.ParseResult<Medication>(createResponse.Content);
+        createResult.Id.Should().NotBeNull();
+
+        var getResponse = await this.httpClient.GetStringAsync($"medications/{createResult.Id}");
+        var savedMedication = await HttpUtils.ParseJson<Medication>(getResponse);
+        savedMedication.Code.Should().BeEquivalentTo(medication.Code);
+        savedMedication.DoseForm.Should().BeEquivalentTo(medication.DoseForm);
+        savedMedication.Ingredient.Should().BeEquivalentTo(medication.Ingredient);
+    }
+
+    private Medication GetTestMedication() => new()
+    {
+        Code = new CodeableConcept
+        {
+            Coding = new List<Coding>
+            {
+                new()
+                {
+                    Code = "400621001",
+                    System = "http://snomed.info/sct",
+                    Display = "Lorazepam 2mg/ml injection solution 1ml vial (product)"
+                }
+            }
+        },
+        DoseForm = new CodeableConcept
+        {
+            Coding = new List<Coding>
+            {
+                new()
+                {
+                    Code = "385219001",
+                    System = "http://snomed.info/sct",
+                    Display = "Injection solution (qualifier value)"
+                }
+            }
+        },
+        Ingredient = new List<Medication.IngredientComponent>
+        {
+            new()
+            {
+                Item = new CodeableReference
+                {
+                    Concept = new CodeableConcept
+                    {
+                        Coding = new List<Coding>
+                        {
+                            new()
+                            {
+                                Code = "387106007",
+                                System = "http://snomed.info/sct",
+                                Display = "Lorazepam (substance)"
+                            }
+                        }
+                    }
+                },
+                Strength = new Ratio
+                {
+                    Numerator = new Quantity
+                    {
+                        Code = "mg",
+                        Value = 2,
+                        System = "http://unitsofmeasure.org"
+                    },
+                    Denominator = new Quantity
+                    {
+                        Code = "mL",
+                        Value = 1,
+                        System = "http://unitsofmeasure.org"
+                    }
+                }
+            }
+        }
+    };
 }
--- test/integration-tests/QMUL.DiabetesBackend.Integration.Tests/TestFixture.cs
+++ test/integration-tests/QMUL.DiabetesBackend.Integration.Tests/TestFixture.cs
@@ -5,18 +5,23 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.DependencyInjection;
 using Model;
+using Utils;
 
 public class TestFixture : WebApplicationFactory<Startup>
 {
+    public const string TestDatabase = "diabetes-test";
+
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         builder.ConfigureServices(services =>
         {
             services.Configure<MongoDatabaseSettings>(options =>
             {
-                options.DatabaseName = "diabetes.test";
-                options.DatabaseConnectionString = "mongodb+srv://localhost:27017/diabetes-test?retryWrites=true&w=majority";
+                options.DatabaseName = "diabetes-test";
+                options.DatabaseConnectionString = "mongodb://localhost:27017/diabetes-test?retryWrites=true&w=majority";
             });
+
+            services.AddSingleton<MongoDBTest>();
         });
     }
 }
