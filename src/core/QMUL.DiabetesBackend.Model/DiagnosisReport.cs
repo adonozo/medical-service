561a8cf17c686efd12995f2d302e3690f48ba50f
adonozo <ar.donozo@acad.ucb.edu.bo>
2024-12-01 20:58:10 +0000

    Add diagnosis report models

---
+++ src/core/QMUL.DiabetesBackend.Model/DiagnosisReport.cs
@@ -0,0 +1,29 @@
+namespace QMUL.DiabetesBackend.Model;
+
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using Enums;
+using FHIR;
+using NodaTime;
+
+public class DiagnosisReport
+{
+    public string Id { get; set; }
+
+    public Status Status { get; set; }
+
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public DiagnosisCategory Category { get; set; }
+
+    public Instant EffectiveTimestamp { get; set; }
+
+    public Instant Issued { get; set; }
+
+    public Reference Subject { get; set; }
+
+    public Reference ResultsInterpreter { get; set; } // TODO 'interpreters service'
+
+    public IReadOnlyList<Reference> Results { get; set; }
+
+    public string Conclusion { get; set; }
+}
